8fa2ec5e082520d0b0c6b44af8da29090960853e
cryofox <hstances@gmail.com>
2018-01-08 21:00:21 -0800

    LWJGL context loaded

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Ryder Stancescu on 1/8/2018.
+ */
+
+import org.lwjgl.Version;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("LWJGL Version " + Version.getVersion() + " is working.");
+    }
+}
---
+++ src/renderEngine/DisplayManager.java
@@ -0,0 +1,19 @@
+package renderEngine;
+
+
+
+/**
+ * Created by Ryder Stancescu on 1/8/2018.
+ */
+public class DisplayManager {
+    public static void createDisplay()
+    {
+
+    }
+
+
+
+    public static void updateDisplay(){}
+
+    public static void closeDisplay(){}
+}
